72974c6de0bfeea7dfb208ec08ac1c88def3a68e
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-07-28 12:37:46 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let createButton = document.getElementById('createButton');
+    let textInput = document.getElementById('new-task-description');
+    createButton.addEventListener('click', function(e) {
+        e.preventDefault();
+        addTask(getValue(textInput));
+        textInput.value = "";
+    });
 });
+
+function addTask(eventValue) {
+    let tasksContainer = document.getElementById('tasks');
+    let appendedChild = addListItem(tasksContainer);
+    appendedChild.innerHTML = eventValue;
+}
+
+function addListItem(parentElement) {
+    let li = document.createElement('li');
+    let appendedChild = parentElement.appendChild(li);
+    return appendedChild;
+}
+
+function getValue(element) {
+    return element.value;
+}
