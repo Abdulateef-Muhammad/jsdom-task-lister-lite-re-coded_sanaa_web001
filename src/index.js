bcd3103ae8f1222675c120c15d04d28a03a304c7
Abdulateef Al-radaee <abdulateef.radaee@gmail.com>
2020-07-30 06:43:25 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -5,21 +5,55 @@ document.addEventListener("DOMContentLoaded", () => {
         e.preventDefault();
         addTask(getValue(textInput));
         textInput.value = "";
+
+        let inputArray = document.getElementsByClassName('delete-input');
+        // console.log(inputArray);
+        addEventListenerToArray(inputArray);
     });
+
+    let inputArray = document.getElementsByClassName('delete-input');
+    // console.log(inputArray);
+    addEventListenerToArray(inputArray);
+
 });
 
 function addTask(eventValue) {
     let tasksContainer = document.getElementById('tasks');
-    let appendedChild = addListItem(tasksContainer);
-    appendedChild.innerHTML = eventValue;
+    let appendedChild = addTaskElement(tasksContainer, eventValue);
 }
 
-function addListItem(parentElement) {
-    let li = document.createElement('li');
-    let appendedChild = parentElement.appendChild(li);
+function addTaskElement(parentElement, eventValue) {
+    let taskId = generateId(6);
+    appendedChild = parentElement;
+    appendedChild.innerHTML += `
+        <li id='${taskId}'>
+            <span>${eventValue} </span>
+            <input id='${taskId}' type='submit' value='delete' class='delete-input'>
+        </li>
+    `;
     return appendedChild;
 }
 
 function getValue(element) {
     return element.value;
+}
+
+function addEventListenerToArray(elementArray) {
+    // console.log(elementArray)
+    for (let i = 0; i < elementArray.length; i++) {
+        elementArray[i].addEventListener('click', function(e) {
+            document.getElementById(e.target.id).remove();
+            // document.querySelector(`#${e.target.id}`).parentNode.removeChild(e.target.id);
+        });
+    }
+}
+
+function generateId(length) {
+    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charsLength = chars.length;
+    let id = '';
+    for (let i = 0; i < length; i++) {
+        id += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return id;
 }
